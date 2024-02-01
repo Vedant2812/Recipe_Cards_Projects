4c6a2a5bcc1642d2e331f1063642797704537c07
Vedant2812 <114892387+Vedant2812@users.noreply.github.com>
2024-02-01 12:46:28 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js - This file needs to be in the root of the directory to work,
 //         so do not move it next to the other scripts
 
-const CACHE_NAME = 'Vedant';
+const CACHE_NAME = 'lab-7-starter';
 
 // Installs the service worker. Feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
@@ -24,17 +24,6 @@ self.addEventListener('activate', function (event) {
 
 // Intercept fetch requests and cache them
 self.addEventListener('fetch', function (event) {
-  // We added some known URLs to the cache above, but tracking down every
-  // subsequent network request URL and adding it manually would be very taxing.
-  // We will be adding all of the resources not specified in the intiial cache
-  // list to the cache as they come in.
-  /*******************************/
-  // This article from Google will help with this portion. Before asking ANY
-  // questions about this section, read this article.
-  // NOTE: In the article's code REPLACE fetch(event.request.url) with
-  //       fetch(event.request)
-  // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
-  /*******************************/
   event.respondWith(caches.open(CACHE_NAME).then(async (cache) => {
     const cachedResponse = await cache.match(event.request);
     if (cachedResponse) {
